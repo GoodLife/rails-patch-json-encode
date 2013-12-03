9af88f892103dd5a14ff7e0b3a776cc0ba2e9016
lulalala <mark@goodlife.tw>
2013-12-03 16:09:32 +0800

    Fix #1 Only patch Rails renderer instead of all base classes. This avoids to_json recursive call when JSON is used.

--- lib/rails/patch/json/encode.rb
+++ lib/rails/patch/json/encode.rb
@@ -1,11 +1,20 @@
 require "rails/patch/json/encode/version"
 
-# Code from http://devblog.agworld.com.au/post/42586025923/the-performance-of-to-json-in-rails-sucks-and-theres
-# essentially reversing Rails' hard-coded call to ActiveSupport::JSON.encode
-[Object, Array, FalseClass, Float, Hash, Integer, NilClass, String, TrueClass].each do |klass|
-  klass.class_eval do
-    def to_json(opts = {})
-      MultiJson::dump(self.as_json(opts), opts)
+# Use multi_json instead of Rails' to_json method (which calls ActiveSupport::JSON)
+# when `render :json => @obj` is called.
+module ActionController
+  module Renderers
+    # Override
+    add :json do |json, options|
+      json = MultiJson::dump(json.as_json(options), options) unless json.kind_of?(String)
+
+      if options[:callback].present?
+        self.content_type ||= Mime::JS
+        "#{options[:callback]}(#{json})"
+      else
+        self.content_type ||= Mime::JSON
+        json
+      end
     end
   end
 end
